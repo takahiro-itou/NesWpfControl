34c87ead56ddd5ca4b14e18798b3f453a57f7a49
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-18 17:55:09 +0900

    空のユーザーコントロールを新規作成

---
+++ Views/GameScreen.xaml.cs
@@ -0,0 +1,10 @@
+﻿//  -*-  coding: utf-8-with-signature-unix;        -*-  //
+
+namespace  NesWpfControl.Views  {
+
+public  partial class  GameScreen : UserControl
+{
+
+}   //  End class  GameScreen
+
+}   //  End of namespace  NesWpfControl.Views
