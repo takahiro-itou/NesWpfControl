f8d07dc5ddb35be2cfc02128767448216bb738cf
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-18 18:34:32 +0900

    コンパイルエラーを修正

--- Views/GameScreen.xaml.cs
+++ Views/GameScreen.xaml.cs
@@ -1,4 +1,19 @@
 ﻿//  -*-  coding: utf-8-with-signature-unix;        -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   NES Debugger Project   ----                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+using System.ComponentModel;
+using System.Windows.Controls;
 
 namespace  NesWpfControl.Views  {
 
