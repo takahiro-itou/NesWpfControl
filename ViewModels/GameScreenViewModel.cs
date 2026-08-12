c10a40e6108e816f163aace8ed85aba1a4fbadc7
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-08-12 23:12:11 +0900

    空のクラスを新規作成

---
+++ ViewModels/GameScreenViewModel.cs
@@ -0,0 +1,53 @@
+﻿//  -*-  coding: utf-8-with-signature-unix;        -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   NES Debugger Project   ----                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+
+namespace  NesWpfControl.ViewModels  {
+
+public  class  GameScreenViewModel
+{
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+
+//========================================================================
+//
+//    Properties.
+//
+
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+
+}   //  End class  GameScreenViewModel
+
+}   //  End of namespace  NesWpfControl.ViewModels
